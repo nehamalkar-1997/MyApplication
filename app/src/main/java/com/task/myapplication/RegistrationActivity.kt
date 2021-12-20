26794ac50fa1b25025668a9f77c88dca799555fa
nehamalkar-1997 <nehamalkar19@gmail.com>
2021-12-20 23:49:39 +0530

    My application

---
+++ app/src/main/java/com/task/myapplication/RegistrationActivity.kt
@@ -0,0 +1,16 @@
+package com.task.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+
+    private fun register(){
+
+     registerButton
+    }
+}
